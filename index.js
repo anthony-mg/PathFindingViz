f680f3382d34b3d1b3d04772ccfa1c5e841e19c4
Anthony <anthonymenardgill@gmail.com>
2022-11-06 16:20:56 -0500

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+function setup() {
+  var canvas = createCanvas(
+    windowWidth - windowWidth * 0.2,
+    windowHeight - windowHeight * 0.2
+  );
+  canvas.parent("canvas-container");
+}
+
+function draw() {
+  background(255, 0, 0);
+}
