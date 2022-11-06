3b8f977abe26126b2c27b8017783cfb837c7ad15
Anthony <anthonymenardgill@gmail.com>
2022-11-06 18:35:49 -0500

    nodes drawn. Spacing between nodes. TODO: check mobile spacings and sizes of nodes. It's acting weird on mobile

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function setup() {
-  var canvas = createCanvas(
-    windowWidth - windowWidth * 0.2,
-    windowHeight - windowHeight * 0.2
-  );
-  canvas.parent("canvas-container");
-}
+const s = (sketch) => {
+  sketch.setup = () => {
+    sketch.nodeSize = 20;
+    sketch.spacing = 3;
+    w = sketch.windowWidth - sketch.windowWidth * 0.2;
+    w = w - (w % 20);
+    h = sketch.windowHeight - sketch.windowHeight * 0.2;
+    h = h - (h % 20);
+    w += (w / sketch.nodeSize) * sketch.spacing + sketch.spacing;
+    h += (h / sketch.nodeSize) * sketch.spacing + sketch.spacing;
+    sketch.canvas = sketch.createCanvas(w, h);
+  };
+  sketch.draw = () => {
+    sketch.background(200, 200, 200);
+    sketch.fill(255, 0, 0);
+    sketch.noStroke();
+    for (var i = sketch.spacing; i < sketch.width; i += sketch.nodeSize + sketch.spacing) {
+      for (var j = sketch.spacing; j < sketch.height; j += sketch.nodeSize + sketch.spacing) {
+        sketch.rect(i, j, sketch.nodeSize, sketch.nodeSize, 5);
+      }
+    }
+  };
 
-function draw() {
-  background(255, 0, 0);
-}
+  sketch.windowResized = () => {
+    w = sketch.windowWidth - sketch.windowWidth * 0.2;
+    w = w - (w % 5);
+    h = sketch.windowHeight - sketch.windowHeight * 0.2;
+    h = h - (h % 5);
+    sketch.resizeCanvas(w, h);
+  };
+};
+
+let p = new p5(s, "canvas-container");
